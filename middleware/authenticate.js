f922b551d0bad543fe3add96aa277a3ac7c15cf8
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-05-13 20:45:51 +0200

    feat: Add authorization for admin
    Implemented middleware for checking if the user has admin status

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -11,4 +11,17 @@ const authenticate = (req, res, next) => {
     }
 };
 
-module.exports = authenticate;
+const authenticateAndAuthorizeAdmin = (req, res, next) => {
+    authenticate(req, res, () => { // Use authenticate middleware first
+        if (req.userData && req.userData.role === "admin") {
+            next(); // User is authenticated and is an admin, proceed to the next middleware
+        } else {
+            return res.status(403).json({ message: "Access denied: requires admin role" });
+        }
+    });
+}
+
+module.exports = {
+    authenticate,
+    authenticateAndAuthorizeAdmin
+};
